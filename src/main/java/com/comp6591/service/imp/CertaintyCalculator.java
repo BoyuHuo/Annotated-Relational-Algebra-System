73ac0b478b8fec9ddc5ef421e7adc600929eaed8
IvyLingZhijing <ivyling063@gmail.com>
2019-11-23 09:36:24 -0500

    add certainty and probability calculator.

--- src/main/java/com/comp6591/service/imp/CertaintyCalculator.java
+++ src/main/java/com/comp6591/service/imp/CertaintyCalculator.java
@@ -1,4 +1,62 @@
 package com.comp6591.service.imp;
 
-public class CertaintyCalculator {
+import com.comp6591.service.CalcultorStrategyInterface;
+import com.comp6591.utils.Constants;
+import com.comp6591.utils.Util;
+
+import java.util.Stack;
+
+public class CertaintyCalculator implements CalcultorStrategyInterface {
+
+    @Override
+    public String getValue(String polynomial) {
+
+        Stack<String> plStack = new Stack<>();
+
+        String[] plItems = polynomial.split(" ");
+
+        for (String s : plItems) {
+
+            if (!s.equals(")")) {
+                plStack.push(s);
+            } else {
+
+                String element1 = plStack.pop().trim();
+                String operator = plStack.pop().trim();
+                String element2 = plStack.pop().trim();
+
+                plStack.pop();
+                plStack.push(certaintyCalculator(element1, operator, element2));
+            }
+        }
+
+        if (plStack.size() != 1) {
+            throw new RuntimeException("Error occurs in calculte the Bag semantics!");
+        } else {
+            return String.valueOf(plStack.pop());
+        }
+    }
+
+
+    private String certaintyCalculator(String element1, String operator, String element2) {
+
+        float certainty1 = strToFloat(element1);
+        float certainty2 = strToFloat(element2);
+        float result;
+
+        if (operator.equals(Constants.DOT)) {
+            result = Math.min(certainty1, certainty2);
+        } else if (operator.equals(Constants.ADD)) {
+            result = Math.max(certainty1, certainty2);
+        } else {
+            throw new RuntimeException("Invailid Operator!");
+        }
+        return String.valueOf(result);
+    }
+
+    private float strToFloat(String str) {
+
+        return Float.valueOf(Util.isTag(str)? Constants.TAGS.get(str) : str);
+    }
+
 }
